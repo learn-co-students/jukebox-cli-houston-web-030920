8df731ff3783702d505488dcc36325bf3fc2db13
willfbren <willfbren@gmail.com>
2020-03-10 11:20:33 -0500

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -33,11 +33,11 @@ def play(songs)
     response = gets.strip
 
     if response.to_i >= 1 && response.to_i <= songs.length
-      puts "Playing #{songs[response.to_i-1]}"
+        puts "Playing #{songs[response.to_i-1]}"
     elsif songs.include?(response)
-      puts "Playing #{songs.find{|song| song == response}}"
+        puts "Playing #{response}"
     else
-      puts "Invalid input, please try again"
+        puts "Invalid input, please try again"
     end
 end
 
