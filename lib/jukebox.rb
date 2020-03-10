a9ab190be982cc1956b5816643eee97292b280e7
bryantomoregie <bryantomoregie@yahoo.com>
2020-03-10 11:11:03 -0500

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,56 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+    puts "I accept the following commands:"
+    puts "- help : displays this help message"
+    puts "- list : displays a list of songs you can play"
+    puts "- play : lets you choose a song to play"
+    puts "- exit : exits this program"
+end
+
+def list(songs)
+    songs.each_with_index do | song, index |
+        puts "#{index + 1}. #{song}"
+    end
+end
+
+def play(songs)
+    puts "Please enter a song name or number:"
+    response = gets.strip 
+    if response.to_i >= 1 && response.to_i <= songs.length 
+        puts "Playing #{songs[response.to_i-1]}"
+    elsif songs.include?(response)
+        puts "Playing #{response}"
+    else 
+        puts "Invalid input, please try again."
+    end
+end 
+
+def exit_jukebox
+    puts "Goodbye"
+end
+
+def run(songs)
+    puts "Please enter a command:"
+    response = gets.strip
+
+    if response == "help"
+        help
+    elsif response == "list"
+        list(songs)
+    elsif response == "play"
+        play(songs)
+    elsif response == "exit"
+        exit_jukebox
+    end
+end
